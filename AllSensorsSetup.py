e4c869dfad879444527582685154329f0c76671c
oShinchan <127603290+oShinchan@users.noreply.github.com>
2023-03-12 17:52:52 +0530

    Update AllSensorsSetup.py

--- AllSensorsSetup.py
+++ AllSensorsSetup.py
@@ -1,10 +1,10 @@
-library for ultrasonic and gas sensor
+library for ultrasonic and gas sensor and servo motor
 
 sudo apt-get install python-rpi.gpio
 data pin connection is in code
 ground is pin number 39 
 5v vcc is pin 2
-for both
+for all 3
 
 
 library for dht11
@@ -15,6 +15,3 @@ Pin 4 is GND (Ground) connected to pin 6 of pi
 
 connected a 10k ohm resistor accros data pin and vcc pin of dht11(pin 1,2)
 https://pimylifeup.com/raspberry-pi-humidity-sensor-dht22/
-
-
-
